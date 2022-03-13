b77574a9329ae5bbc7477ec3ac7ae9db3f25e228
Hieu Nguyen <hieund898@gmail.com>
2022-03-13 15:59:47 +0700

    Thực hành unit test

---
+++ ThucHanh-UnitTest/Calculator/Calculator/Form1.Designer.cs
@@ -0,0 +1,197 @@
+﻿namespace Calculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxNum1 = new System.Windows.Forms.TextBox();
+            this.textBoxNum2 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBoxResult = new System.Windows.Forms.TextBox();
+            this.buttonAdd = new System.Windows.Forms.Button();
+            this.buttonMinus = new System.Windows.Forms.Button();
+            this.buttonMul = new System.Windows.Forms.Button();
+            this.buttonDiv = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Maroon;
+            this.label1.Location = new System.Drawing.Point(61, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(258, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "CHƯƠNG TRÌNH TÍNH TOÁN ĐƠN GIẢN";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.buttonDiv);
+            this.groupBox1.Controls.Add(this.buttonMul);
+            this.groupBox1.Controls.Add(this.buttonMinus);
+            this.groupBox1.Controls.Add(this.buttonAdd);
+            this.groupBox1.Controls.Add(this.textBoxResult);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.textBoxNum2);
+            this.groupBox1.Controls.Add(this.textBoxNum1);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Location = new System.Drawing.Point(64, 93);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(255, 244);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Tính toán đơn giản";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(18, 39);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(62, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Số thứ nhất";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(18, 81);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(55, 13);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Số thứ hai";
+            // 
+            // textBoxNum1
+            // 
+            this.textBoxNum1.Location = new System.Drawing.Point(86, 36);
+            this.textBoxNum1.Name = "textBoxNum1";
+            this.textBoxNum1.Size = new System.Drawing.Size(142, 20);
+            this.textBoxNum1.TabIndex = 2;
+            // 
+            // textBoxNum2
+            // 
+            this.textBoxNum2.Location = new System.Drawing.Point(86, 81);
+            this.textBoxNum2.Name = "textBoxNum2";
+            this.textBoxNum2.Size = new System.Drawing.Size(142, 20);
+            this.textBoxNum2.TabIndex = 3;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(18, 123);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(44, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Kết quả";
+            // 
+            // textBoxResult
+            // 
+            this.textBoxResult.Location = new System.Drawing.Point(86, 120);
+            this.textBoxResult.Name = "textBoxResult";
+            this.textBoxResult.ReadOnly = true;
+            this.textBoxResult.Size = new System.Drawing.Size(142, 20);
+            this.textBoxResult.TabIndex = 5;
+            // 
+            // buttonAdd
+            // 
+            this.buttonAdd.Location = new System.Drawing.Point(21, 171);
+            this.buttonAdd.Name = "buttonAdd";
+            this.buttonAdd.Size = new System.Drawing.Size(41, 23);
+            this.buttonAdd.TabIndex = 6;
+            this.buttonAdd.Text = "Cộng";
+            this.buttonAdd.UseVisualStyleBackColor = true;
+            this.buttonAdd.Click += new System.EventHandler(this.buttonAdd_Click);
+            // 
+            // buttonMinus
+            // 
+            this.buttonMinus.Location = new System.Drawing.Point(77, 171);
+            this.buttonMinus.Name = "buttonMinus";
+            this.buttonMinus.Size = new System.Drawing.Size(41, 23);
+            this.buttonMinus.TabIndex = 7;
+            this.buttonMinus.Text = "Trừ";
+            this.buttonMinus.UseVisualStyleBackColor = true;
+            this.buttonMinus.Click += new System.EventHandler(this.buttonMinus_Click);
+            // 
+            // buttonMul
+            // 
+            this.buttonMul.Location = new System.Drawing.Point(131, 171);
+            this.buttonMul.Name = "buttonMul";
+            this.buttonMul.Size = new System.Drawing.Size(41, 23);
+            this.buttonMul.TabIndex = 8;
+            this.buttonMul.Text = "Nhân";
+            this.buttonMul.UseVisualStyleBackColor = true;
+            this.buttonMul.Click += new System.EventHandler(this.buttonMul_Click);
+            // 
+            // buttonDiv
+            // 
+            this.buttonDiv.Location = new System.Drawing.Point(187, 171);
+            this.buttonDiv.Name = "buttonDiv";
+            this.buttonDiv.Size = new System.Drawing.Size(41, 23);
+            this.buttonDiv.TabIndex = 9;
+            this.buttonDiv.Text = "Chia";
+            this.buttonDiv.UseVisualStyleBackColor = true;
+            this.buttonDiv.Click += new System.EventHandler(this.buttonDiv_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(374, 409);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox textBoxResult;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBoxNum2;
+        private System.Windows.Forms.TextBox textBoxNum1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button buttonDiv;
+        private System.Windows.Forms.Button buttonMul;
+        private System.Windows.Forms.Button buttonMinus;
+        private System.Windows.Forms.Button buttonAdd;
+    }
+}
+
---
+++ ThucHanh-UnitTest/Calculator/Calculator/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static Calculator.Calculation;
+
+namespace Calculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonAdd_Click(object sender, EventArgs e)
+        {
+            Calculation cal = new Calculation(int.Parse(textBoxNum1.Text), int.Parse(textBoxNum2.Text));
+            textBoxResult.Text = cal.Execute("+").ToString();
+        }
+
+        private void buttonMinus_Click(object sender, EventArgs e)
+        {
+            Calculation cal = new Calculation(int.Parse(textBoxNum1.Text), int.Parse(textBoxNum2.Text));
+            textBoxResult.Text = cal.Execute("-").ToString();
+        }
+
+        private void buttonMul_Click(object sender, EventArgs e)
+        {
+            Calculation cal = new Calculation(int.Parse(textBoxNum1.Text), int.Parse(textBoxNum2.Text));
+            textBoxResult.Text = cal.Execute("*").ToString();
+        }
+
+        private void buttonDiv_Click(object sender, EventArgs e)
+        {
+            Calculation cal = new Calculation(int.Parse(textBoxNum1.Text), int.Parse(textBoxNum2.Text));
+            textBoxResult.Text = cal.Execute("/").ToString();
+        }
+    }
+}
